53f69f18380f938ed63666cff8fa23f34e67346b
krishnanunnijp <krishnanunni.j@nestgroup.net>
2022-11-29 10:10:46 +0530

    us added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PassengerViewComponent } from './passenger-view/passenger-view.component';
+import { UsViewComponent } from './us-view/us-view.component';
 
 const myRouter:Routes=[
   {
     path:"view",
     component: PassengerViewComponent
+  },
+  {
+    path:"usview",
+    component: UsViewComponent
   }
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    PassengerViewComponent
+    PassengerViewComponent,
+    UsViewComponent
   ],
   imports: [
     BrowserModule,
