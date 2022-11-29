3c5c259b5d007ef3c781731b2f2110011ff232bc
krishnanunnijp <krishnanunni.j@nestgroup.net>
2022-11-29 09:34:32 +0530

    passengers added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PassengerViewComponent } from './passenger-view/passenger-view.component';
+
+const myRouter:Routes=[
+  {
+    path:"view",
+    component: PassengerViewComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PassengerViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRouter)
   ],
   providers: [],
   bootstrap: [AppComponent]
